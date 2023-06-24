df843900b9fce3ad0a0a760470e77559a162c1e8
SofiaSJ09 <sjimenezsofia@gmail.com>
2023-06-24 11:27:35 -0600

    Proyecto Final

---
+++ ProyectoFinal.js
@@ -0,0 +1,29 @@
+//Ejemplo recursivo
+var res = fibonacci(3);
+console.log(res);
+
+function fibonacci(n){
+    if( n <= 0 ){
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci (n - 2);
+    }
+}
+
+
+
+  //Interativo ejemplo 3
+
+  function fibonacciInteractivo(n)
+{
+    let fib = [0,1];
+
+    for(let i = 2; i <= n; i++)
+    {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    return fib[n];
+}
